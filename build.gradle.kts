54ef5e670ec3b2ccb1b98436d856a03115431878
ygdrasil-io <alexandre.mommers@gmail.com>
2024-10-14 01:43:46 +0200

    Update default project version to 0.0.1-SNAPSHOT
    Previously, the default version was set to "0.0.0-SNAPSHOT" if no VERSION environment variable was found. This change updates the default version to "0.0.1-SNAPSHOT" to reflect incremental progress.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ allprojects {
 	}
 
 	group = "io.ygdrasil"
-	version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "0.0.0-SNAPSHOT"
+	version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "0.0.1-SNAPSHOT"
 }
 
 
